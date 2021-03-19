3b1c5e223b33139c7a8339e166a7e9acb8053ec5
Christian Payan <payanstocks@gmail.com>
2021-03-19 14:32:42 -0700

    Fully working code without final acceptance criteria

--- script.js
+++ script.js
@@ -2,57 +2,69 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
 
   //Major text block 
+
   var passwordText = document.querySelector("#password");
+
   var passwordLength = prompt("Enter the lenght of the password you would like to generate. \nMust be between 8 charectors and 128 charectors.");
 
 
   if(passwordLength > 7 && passwordLength < 129) {
-    var confirmLowerCase = confirm("Do you want Lowercase charectors?");
 
-    var confirmUpperCase = confirm("Do you want Uppercase charectors?");
-
-    var confirmNumbers = confirm("Do you want Numbers charectors?");
-
-    var confirmSpecial = confirm("Do you want Special charectors?");
-
-    // 
-    
     var collectionOfCriterias = "";
     var lowerCase = "abcdefghijklmnopqrstuvwxyz";
     var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var numbers = "1234567890";
     var special = "~!@#$%^&*()-_=+<>?";
-    var generatePassword = "";
-    //Condition
+    var generatedPassword = "";
+    // let filter = [{confirmLowerCase}, {confirmUpperCase}, {confirmNumbers}, {confirmSpecial}].filter(item => Object.values(item)[0]);
+    // var length = passwordLength - filter.length;
+    function addNecessaryChars (charecters) {
+      collectionOfCriterias += charecters;
+      const addNecessaryChar = Math.floor(Math.random() * charecters.length);
+      const adding = (charecters.charAt(addNecessaryChar));
+      generatedPassword = generatedPassword + adding;
+    }
+    var confirmLowerCase = confirm("Do you want Lowercase charecters?");
+
+    var confirmUpperCase = confirm("Do you want Uppercase charecters?");
+
+    var confirmNumbers = confirm("Do you want Numbers charecters?");
+
+    var confirmSpecial = confirm("Do you want Special charecters?");
 
     if (confirmLowerCase || confirmUpperCase || confirmNumbers || confirmSpecial) {
+      
       if (confirmLowerCase) {
-        collectionOfCriterias += lowerCase;
+        addNecessaryChars(lowerCase);
       }
       if (confirmUpperCase) {
-        collectionOfCriterias += upperCase;
+        addNecessaryChars(upperCase);
       }
       if (confirmNumbers) {
-        collectionOfCriterias += numbers;
+        addNecessaryChars(numbers);
       }
       if (confirmSpecial) {
-        collectionOfCriterias += special;
+        addNecessaryChars(special);
       }
 
-      for (i = 0; i < passwordLength; i++) {
+      while (generatedPassword.length < passwordLength) {
         var random = (Math.floor(Math.random() * collectionOfCriterias.length));
         var result = (collectionOfCriterias.charAt(random));
-        generatePassword = generatePassword + result;
+        generatedPassword = generatedPassword + result;
       }
-      passwordText.textContent = generatePassword;
-      console.log(generatePassword);
+      // generatedPassword = generatePassword(confirmLowerCase, confirmUpperCase, confirmNumbers, confirmSpecial);
 
+      passwordText.textContent = generatedPassword;
+      // console.log(generatePassword);
+      // console.log(filter);
     } else {
       alert("Try again !! Password must contain atleast one criteria.")
-  }
+    }
+
 } else {
   alert("Try again !! Password must be between 8 and 128.. No more.. No less.")
 }
